5d6b188ffa6cebe4d91aae81ff84ac778b154814
Wolfram Hempel <hempel.wolfram@gmail.com>
2016-10-23 13:07:44 +0200

    Added creation and deletion

--- src/connector.js
+++ src/connector.js
@@ -3,6 +3,7 @@
 const EventEmitter = require( 'events' ).EventEmitter;
 const AWS = require( 'aws-sdk' );
 const pkg = require( '../package.json' );
+const dataTransform = require( './transform-data' );
 
 /**
  *
@@ -24,28 +25,20 @@ module.exports = class DynamoDbConnector extends EventEmitter
 		this.name = pkg.name;
 		this.version = pkg.version;
 
-		// Set region
-		if (options.region) {
-			AWS.config.update({
-				region: options.region
-			});
+		if (!options.region) {
+			throw new Error( 'options.region is required' );
 		}
 
-		// Set table
-		if (!options.table) {
-			throw new Error('"table" option is required');
-		}
-		this.table = options.table;
+		AWS.config.update({
+			region: options.region
+		});
 
-		// Set DynamoDb instance
-		if (!options.db) {
-			options.db = new AWS.DynamoDB.DocumentClient();
-		}
-		this.db = options.db;
 
+		this.db = new AWS.DynamoDB();
+		this.documentClient = options.documentClient || new AWS.DynamoDB.DocumentClient();
 	}
 
-	createTable( tableName, callback ) {
+	createTable( tableName, callback, dontWait ) {
 		var params = {
 			AttributeDefinitions: [{
 					AttributeName: 'ds_id',
@@ -59,15 +52,51 @@ module.exports = class DynamoDbConnector extends EventEmitter
 				ReadCapacityUnits: 5,
 				WriteCapacityUnits: 5
 			},
-			TableName: tableName
-		 
-		 
-			// StreamSpecification: {
-			//   StreamEnabled: true || false,
-			//   StreamViewType: 'NEW_IMAGE | OLD_IMAGE | NEW_AND_OLD_IMAGES | KEYS_ONLY'
-			// }
+			TableName: tableName,
+
+			StreamSpecification: {
+			  StreamEnabled: true,
+			  StreamViewType: 'NEW_AND_OLD_IMAGES'
+			}
 		};
-			( new AWS.DynamoDB() ).createTable(params, callback );
+
+		this.db.createTable( params, ( err, data ) => {
+			if( err ) {
+				callback( err );
+				return;
+			}
+
+			if( dontWait === true ) {
+				callback( null, data );
+				return;
+			}
+
+			this.db.waitFor('tableExists', { TableName: tableName }, function( err, data ){
+				callback( err, data );
+			});
+		});
+	}
+
+	deleteTable( tableName, callback, dontWait ) {
+		var params = {
+			TableName : tableName
+		};
+
+		this.db.deleteTable( params, ( err, data ) => {
+			if( err ) {
+				callback( err );
+				return;
+			}
+
+			if( dontWait === true ) {
+				callback( null, data );
+				return;
+			}
+
+			this.db.waitFor( 'tableNotExists', { TableName: tableName }, function( err, data ){
+				callback( err, data );
+			});
+		});
 	}
 
 	/**
@@ -81,13 +110,13 @@ module.exports = class DynamoDbConnector extends EventEmitter
 	 * @returns {void}
 	 */
 	set(id, data, cb) {
-		data.ds_id = id;
+
 		const params = {
 			TableName: this.table,
-			Item: data
+			Item: dataTransform.transformValueForStorage( data )
 		};
 		// console.log('Set', params);
-		this.db.put(params, err => {
+		this.documentClient.put(params, err => {
 			cb(err ? err.message : null);
 		});
 	}
@@ -109,16 +138,16 @@ module.exports = class DynamoDbConnector extends EventEmitter
 				ds_id: id
 			}
 		};
-		// console.log('Get', params);
-		this.db.get(params, (err, res) => {
+
+		this.documentClient.get(params, (err, res) => {
 			if (err) {
 				return cb(err.message);
 			}
 			if (!res || !res.Item) {
 				return cb(null, null);
 			}
-			delete res.Item.ds_id;
-			return cb(null, res.Item)
+
+			return cb(null, dataTrasform.transformValueFromStorage( res.Item ) );
 		});
 	}
 
@@ -139,7 +168,7 @@ module.exports = class DynamoDbConnector extends EventEmitter
 				ds_id: id
 			}
 		};
-		this.db.delete(params, err => {
+		this.documentClient.delete(params, err => {
 			cb(err ? err.message : null);
 		});
 	}
--- test/cache-connectorSpec.js
+++ test/cache-connectorSpec.js
@@ -4,79 +4,112 @@
 const expect = require('chai').expect
 const CacheConnector = require('../src/connector')
 const EventEmitter = require('events').EventEmitter
+
 const settings = {
-  'region': 'eu-central-1',
-  'table': 'test-table'
+	'region': 'eu-central-1'
 }
-const MESSAGE_TIME = 20
 
-describe.only( 'the message connector has the correct structure', () => {
-  var cacheConnector
+const MESSAGE_TIME = 200
 
-  it( 'throws an error if required connection parameters are missing', () => {
-    expect( () => { new CacheConnector( 'gibberish' ) } ).to.throw()
-  })
+describe( 'the message connector has the correct structure', () => {
+	var cacheConnector
 
-  it( 'creates the cacheConnector', () => {
-    cacheConnector = new CacheConnector( settings )
-    expect( cacheConnector.isReady ).to.equal( true )
-  })
+	it( 'throws an error if required connection parameters are missing', () => {
+		expect( () => { new CacheConnector( 'gibberish' ) } ).to.throw()
+	})
 
-  it( 'creates a table', ( done ) => {
-    cacheConnector.createTable( 'some-test-table', ( err, data ) => {
-      if( err ) {
-        throw err;
-      } else {
-        done();
-      }
-    });
-  });
+	it( 'creates the cacheConnector', () => {
+		cacheConnector = new CacheConnector( settings )
+		expect( cacheConnector.isReady ).to.equal( true )
+	})
 
-  // it( 'implements the cache/storage connector interface', () =>  {
-    // expect( cacheConnector.name ).to.be.a( 'string' )
-    // expect( cacheConnector.version ).to.be.a( 'string' )
-    // expect( cacheConnector.get ).to.be.a( 'function' )
-    // expect( cacheConnector.set ).to.be.a( 'function' )
-    // expect( cacheConnector.delete ).to.be.a( 'function' )
-    // expect( cacheConnector instanceof EventEmitter ).to.equal( true )
-  // })
+	it( 'implements the cache/storage connector interface', () =>  {
+		expect( cacheConnector.name ).to.be.a( 'string' )
+		expect( cacheConnector.version ).to.be.a( 'string' )
+		expect( cacheConnector.get ).to.be.a( 'function' )
+		expect( cacheConnector.set ).to.be.a( 'function' )
+		expect( cacheConnector.delete ).to.be.a( 'function' )
+		expect( cacheConnector.createTable ).to.be.a( 'function' )
+		expect( cacheConnector.deleteTable ).to.be.a( 'function' )
+		expect( cacheConnector instanceof EventEmitter ).to.equal( true )
+	});
+});
 
-  // it( 'retrieves a non existing value', ( done ) => {
-    // cacheConnector.get( 'someValue', ( error, value ) => {
-      // expect( error ).to.equal( null )
-      // expect( value ).to.equal( null )
-      // done()
-    // })
-  // })
+describe( 'creates and deletes tables', function() {
+	var cacheConnector;
+	var TABLE_NAME = 'some-test-table';
 
-  // it( 'sets a value', ( done ) => {
-    // cacheConnector.set( 'someValue', {  _d: { v: 10 }, firstname: 'Wolfram' }, ( error ) => {
-      // expect( error ).to.equal( null )
-      // done()
-    // })
-  // })
+	this.timeout( 100000 );
 
-  // it( 'retrieves an existing value', ( done ) => {
-    // cacheConnector.get( 'someValue', ( error, value ) => {
-      // expect( error ).to.equal( null )
-      // expect( value ).to.deep.equal( {  _d: { v: 10 }, firstname: 'Wolfram' } )
-      // done()
-    // })
-  // })
+	it( 'creates the cacheConnector', () => {
+		cacheConnector = new CacheConnector( settings )
+		expect( cacheConnector.isReady ).to.equal( true )
+	})
 
-  // it( 'deletes a value', ( done ) => {
-    // cacheConnector.delete( 'someValue', ( error ) => {
-      // expect( error ).to.equal( null )
-      // done()
-    // })
-  // })
+	it( 'creates a table', ( done ) => {
+		cacheConnector.createTable( TABLE_NAME, ( err, data ) => {
+			expect( err ).to.be.null;
+			done();
+		});
+	});
 
-  // it( 'Can\'t retrieve a deleted value', ( done ) => {
-    // cacheConnector.get( 'someValue', ( error, value ) => {
-      // expect( error ).to.equal( null )
-      // expect( value ).to.equal( null )
-      // done()
-    // })
-  // })
+	it( 'fails when trying to create an existing table', ( done ) => {
+		cacheConnector.createTable( TABLE_NAME, ( err, data ) => {
+			expect( err ).to.not.be.null;
+			done();
+		});
+	});
 
-})
+	it( 'deletes a table', ( done ) => {
+		cacheConnector.deleteTable( TABLE_NAME, ( err, data ) => {
+			expect( err ).to.be.null;
+			done();
+		});
+	});
+
+	it( 'fails when trying to delete a table a second time', ( done ) => {
+		cacheConnector.deleteTable( TABLE_NAME, ( err, data ) => {
+			expect( err ).to.not.be.null;
+			done();
+		});
+	});
+});
+
+
+	// it( 'retrieves a non existing value', ( done ) => {
+		// cacheConnector.get( 'someValue', ( error, value ) => {
+			// expect( error ).to.equal( null )
+			// expect( value ).to.equal( null )
+			// done()
+		// })
+	// })
+
+	// it( 'sets a value', ( done ) => {
+		// cacheConnector.set( 'someValue', {  _d: { v: 10 }, firstname: 'Wolfram' }, ( error ) => {
+			// expect( error ).to.equal( null )
+			// done()
+		// })
+	// })
+
+	// it( 'retrieves an existing value', ( done ) => {
+		// cacheConnector.get( 'someValue', ( error, value ) => {
+			// expect( error ).to.equal( null )
+			// expect( value ).to.deep.equal( {  _d: { v: 10 }, firstname: 'Wolfram' } )
+			// done()
+		// })
+	// })
+
+	// it( 'deletes a value', ( done ) => {
+		// cacheConnector.delete( 'someValue', ( error ) => {
+			// expect( error ).to.equal( null )
+			// done()
+		// })
+	// })
+
+	// it( 'Can\'t retrieve a deleted value', ( done ) => {
+		// cacheConnector.get( 'someValue', ( error, value ) => {
+			// expect( error ).to.equal( null )
+			// expect( value ).to.equal( null )
+			// done()
+		// })
+	// })
